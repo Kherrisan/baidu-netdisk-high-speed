90f61a0a359ec0ad2191864834bb0df5abecc6dd
Liang Ding <dl88250@gmail.com>
2017-12-25 00:41:40 +0800

    :art: 多种情况处理

--- request.js
+++ request.js
@@ -2,16 +2,35 @@ chrome.webRequest.onBeforeRequest.addListener(
   function (details) {
     console.log(details);
     const xhr = new XMLHttpRequest();
-    xhr.open("GET", details.url, false);
-    xhr.send();
+    xhr.open(details.method, details.url, false);
+    if ("POST" === details.method) {
+      xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+      var params = "";
+      for (var name in details.requestBody.formData) {
+        params += name + "=" + details.requestBody.formData[name] + "&"
+      }
+      xhr.send(params);
+    } else {
+      xhr.send();
+    }
     const result = JSON.parse(xhr.responseText);
     console.log(result);
+    if (0 !== result.errno) {
+      return {cancel: false};
+    }
+
+    console.log(result.list);
     var url = "";
-    if (result.dlink[0]) {
-      url = result.dlink[0].dlink;
-    } else {
+    if (result.dlink) {
+      if (result.dlink instanceof Array) {
+        url = result.dlink[0].dlink;
+      } else {
+        url = result.dlink;
+      }
+    } else if (result.list) {
       url = result.list[0].dlink;
     }
+
     chrome.tabs.create({
       'url': chrome.extension.getURL("url.html"),
       'active': true
@@ -19,7 +38,6 @@ chrome.webRequest.onBeforeRequest.addListener(
       var selfTabId = tab.id;
       chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
         if (changeInfo.status == "complete" && tabId == selfTabId) {
-          // send the data to the page's script:
           var tabs = chrome.extension.getViews({type: "tab"});
           tabs[0].setURL(url);
         }
@@ -28,6 +46,6 @@ chrome.webRequest.onBeforeRequest.addListener(
 
     return {cancel: true};
   },
-  {urls: ["*://*.baidu.com/api/download?*", "*://*.baidu.com/api/sharedownload?*"]},
-  ["blocking"]
+  {urls: ["*://*.baidu.com/api/*download?*"]},
+  ["blocking", "requestBody"]
 );
