a4711e400dc7c63764ea97fccc453cf18a91063f
Liang Ding <dl88250@gmail.com>
2017-12-24 22:33:36 +0800

    :art: 分享链接

--- request.js
+++ request.js
@@ -1,16 +1,24 @@
 chrome.webRequest.onBeforeRequest.addListener(
-  function(details) {
+  function (details) {
+    console.log(details);
     const xhr = new XMLHttpRequest();
     xhr.open("GET", details.url, false);
     xhr.send();
     const result = JSON.parse(xhr.responseText);
-    const url = result.dlink[0].dlink;
-
-    chrome.tabs.create({'url': chrome.extension.getURL("url.html"),
-      'active': true}, function(tab){
+    console.log(result);
+    var url = "";
+    if (result.dlink[0]) {
+      url = result.dlink[0].dlink;
+    } else {
+      url = result.list[0].dlink;
+    }
+    chrome.tabs.create({
+      'url': chrome.extension.getURL("url.html"),
+      'active': true
+    }, function (tab) {
       var selfTabId = tab.id;
-      chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-        if (changeInfo.status == "complete" && tabId == selfTabId){
+      chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+        if (changeInfo.status == "complete" && tabId == selfTabId) {
           // send the data to the page's script:
           var tabs = chrome.extension.getViews({type: "tab"});
           tabs[0].setURL(url);
@@ -18,8 +26,8 @@ chrome.webRequest.onBeforeRequest.addListener(
       });
     });
 
-    return { cancel: true };
+    return {cancel: true};
   },
-  {urls: ["*://*.baidu.com/api/download?*"]},
+  {urls: ["*://*.baidu.com/api/download?*", "*://*.baidu.com/api/sharedownload?*"]},
   ["blocking"]
 );
