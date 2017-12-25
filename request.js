3e366ef1030c88cbeff71e5d8380f7bf70be3905
Van <lly219@gmail.com>
2017-12-26 00:40:57 +0800

    :lipstick:

--- request.js
+++ request.js
@@ -19,6 +19,7 @@ chrome.webRequest.onBeforeRequest.addListener(
     }
 
     var url = "";
+    // TODO https://greasyfork.org/zh-CN/scripts/23635-%E7%99%BE%E5%BA%A6%E7%BD%91%E7%9B%98%E7%9B%B4%E6%8E%A5%E4%B8%8B%E8%BD%BD%E5%8A%A9%E6%89%8B/code
     if (result.dlink) {
       if (result.dlink instanceof Array) {
         url = result.dlink[0].dlink;
