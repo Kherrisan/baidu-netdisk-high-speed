156403a91068a5f7272a47535cfce52b8af8d8f6
Liang Ding <dl88250@gmail.com>
2017-12-26 15:56:20 +0800

    :art: tab 重复打开判断

--- request.js
+++ request.js
@@ -2,17 +2,27 @@ var gCurrentTab = null;
 
 chrome.webRequest.onBeforeRequest.addListener(
   function (details) {
-    if (0 <= details.url.indexOf("baidupcs.com/") && 0 <= details.url.indexOf("sign=")) {
-      chrome.tabs.create({
-        'url': chrome.extension.getURL("url.html"),
-        'active': true
-      }, function (tab) {
-        var selfTabId = tab.id;
-        chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-          if (changeInfo.status == "complete" && tabId == selfTabId) {
-            var tabs = chrome.extension.getViews({type: "tab"});
-            tabs[0].setURL(details.url);
-          }
+    console.log(details.url)
+    if (0 <= details.url.indexOf("baidupcs.com") && 0 <= details.url.indexOf("/file")) {
+      chrome.tabs.query({title: "获取下载链接成功！"}, function (tabs) {
+        var tabIds = [];
+        for (var i in tabs) {
+          tabIds.push(tabs[i].id);
+        }
+
+        chrome.tabs.remove(tabIds, function () {
+          chrome.tabs.create({
+            'url': chrome.extension.getURL("url.html"),
+            'active': true
+          }, function (tab) {
+            var selfTabId = tab.id;
+            chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+              if (changeInfo.status == "complete" && tabId == selfTabId) {
+                var tabs = chrome.extension.getViews({type: "tab"});
+                tabs[0].setURL(details.url);
+              }
+            });
+          });
         });
       });
     }
