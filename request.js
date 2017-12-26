ac0ea702f577740dc467562f12461417a004d24a
Liang Ding <dl88250@gmail.com>
2017-12-26 14:18:56 +0800

    :art: 通过调试器获取响应

--- request.js
+++ request.js
@@ -1,53 +1,91 @@
+var gCurrentTab = null;
+
 chrome.webRequest.onBeforeRequest.addListener(
   function (details) {
-    const xhr = new XMLHttpRequest();
-    xhr.open(details.method, details.url, false);
-    if ("POST" === details.method) {
-      xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-      var params = "";
-      for (var name in details.requestBody.formData) {
-        params += name + "=" + details.requestBody.formData[name] + "&"
-      }
-      xhr.send(params);
-    } else {
-      xhr.send();
-    }
-    const result = JSON.parse(xhr.responseText);
-
-    if (0 !== result.errno) {
+    if (0 > details.url.indexOf("/api/download") && 0 > details.url.indexOf("/api/sharedownload")) {
       return {cancel: false};
     }
 
-    var url = "";
-    // TODO https://greasyfork.org/zh-CN/scripts/23635-%E7%99%BE%E5%BA%A6%E7%BD%91%E7%9B%98%E7%9B%B4%E6%8E%A5%E4%B8%8B%E8%BD%BD%E5%8A%A9%E6%89%8B/code
-    if (result.dlink) {
-      if (result.dlink instanceof Array) {
-        url = result.dlink[0].dlink;
-      } else {
-        url = result.dlink;
-      }
-    } else if (result.list && result.list instanceof Array) {
-        url = result.list[0].dlink;
-    }
-    if(url === '' || !url) {
-      return {cancel: false};
-    }
-
-    chrome.tabs.create({
-      'url': chrome.extension.getURL("url.html"),
-      'active': true
-    }, function (tab) {
-      var selfTabId = tab.id;
-      chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-        if (changeInfo.status == "complete" && tabId == selfTabId) {
-          var tabs = chrome.extension.getViews({type: "tab"});
-          tabs[0].setURL(url);
-        }
+    if (!gCurrentTab) {
+      chrome.tabs.query({
+        currentWindow: true,
+        active: true
+      }, function (tabs) {
+        gCurrentTab = tabs[0];
+        chrome.debugger.attach({
+          tabId: gCurrentTab.id
+        }, "1.0", function () {
+          chrome.debugger.sendCommand({
+            tabId: gCurrentTab.id
+          }, "Network.enable");
+        });
       });
-    });
+    }
 
-    return {cancel: true};
+    return {cancel: false};
   },
-  {urls: ["*://*.baidu.com/api/*download?*"]},
+  {urls: ["*://*.baidu.com/api/*"]},
   ["blocking", "requestBody"]
+);
+
+chrome.debugger.onEvent.addListener(function (source, method, params) {
+    if (gCurrentTab.id != source.tabId) {
+      return;
+    }
+
+    if (method == "Network.loadingFinished") {
+      var tabId = source.tabId;
+      var requestId = params.requestId;
+      chrome.debugger.sendCommand(
+        source,
+        "Network.getResponseBody",
+        {"requestId": requestId},
+        function (res) {
+          if (!res || !res.body || 0 > res.body.indexOf("errno")) {
+            return;
+          }
+          const result = JSON.parse(res.body);
+          if (0 !== result.errno) {
+            return;
+          }
+
+          var url = "";
+          if (result.dlink) {
+            if (result.dlink instanceof Array) {
+              url = result.dlink[0].dlink;
+            } else {
+              url = result.dlink;
+            }
+          } else if (result.list && result.list instanceof Array) {
+            url = result.list[0].dlink;
+          }
+          if (url === '' || !url) {
+            return {cancel: false};
+          }
+
+          console.log("got dlink: ", url)
+          chrome.debugger.detach(source);
+          gCurrentTab = null;
+
+          chrome.tabs.create({
+            'url': chrome.extension.getURL("url.html"),
+            'active': true
+          }, function (tab) {
+            var selfTabId = tab.id;
+            chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+              if (changeInfo.status == "complete" && tabId == selfTabId) {
+                var tabs = chrome.extension.getViews({type: "tab"});
+                tabs[0].setURL(url);
+              }
+            });
+          });
+        });
+    }
+  }
+);
+
+chrome.webRequest.onResponseStarted.addListener(function (details) {
+    console.log(details);
+  },
+  {urls: ["*://*.baidu.com/api/sharedownload*", "*://*.baidu.com/api/download*"]}
 );
